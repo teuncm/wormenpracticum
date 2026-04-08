26625c762bb2015ab126da3aa5340ffa4fba1dc3
teuncm <teun.c.mathijssen@gmail.com>
2026-04-08 15:34:30 +0200

    Adapt dummy data generator to new table format

--- tools/worm_data_gen.py
+++ tools/worm_data_gen.py
@@ -8,38 +8,35 @@ from app.model.data_io import (
 )
 
 
-def gen_dummy_data(t_max, sample_rate, num_stims, num_resps) -> pd.DataFrame:
+def gen_dummy_data(
+    t_max, sample_rate, num_i_chan, num_o_chan, num_stims
+) -> pd.DataFrame:
     """Generate matrix with 16 sine waves for testing."""
     # Total number of samples
     N = sgn.quantize_time_point(time_s=t_max, sr_hz=sample_rate)
     timestamps = np.linspace(0, t_max, num=N, endpoint=False)
 
-    stims = np.array([(0 * timestamps) for _ in range(num_stims)])
-    resps = np.array([(0 * timestamps) for _ in range(num_resps)])
+    t = pd.Series(timestamps, name="t (s)")
 
-    # stims += np.random.normal(scale=0.1, size=waves.shape)
+    stim_list = []
+    for i in range(num_stims):
+        for j in range(num_i_chan):
+            signal = 0 * timestamps
+            stim_list.append(pd.Series(signal, name=f"s{i}_i{j} (V)"))
 
-    data = np.array(
-        [timestamps]
-        + [stims[i] for i in range(num_stims)]
-        + [resps[i] for i in range(num_resps)]
-    ).T
+        for j in range(num_o_chan):
+            signal = 0 * timestamps
+            stim_list.append(pd.Series(signal, name=f"s{i}_o{j} (V)"))
 
-    df = pd.DataFrame(
-        data,
-        columns=["Timestamp (s)"]
-        + [f"Channel {i} (V)" for i in range(num_stims + num_resps)],
-    )
+    df = pd.concat([t, *stim_list], axis=1)
 
     return df
 
 
 def main():
     dummy_df = gen_dummy_data(
-        t_max=0.0010, sample_rate=100000, num_stims=1, num_resps=1
+        t_max=0.0010, sample_rate=10000, num_i_chan=2, num_o_chan=2, num_stims=3
     )
-
-    print(dummy_df)
 
     file_path = Path("data/test_data.parquet")
     if file_path:
