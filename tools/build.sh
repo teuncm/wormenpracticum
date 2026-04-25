5d0bb56d76bc00695a4418e0ef1fd9bf1730275a
teuncm <teun.c.mathijssen@gmail.com>
2026-04-25 18:47:03 +0200

    Change macos build script to build to one file

--- tools/build.sh
+++ tools/build.sh
@@ -1,2 +1,2 @@
 # Mac/Linux build script
-uv run pyinstaller --icon app/window/icon.ico --noconfirm --windowed --name wormenpracticum --add-data "app/window:app/window" main.py
+uv run pyinstaller --icon app/window/icon.ico --noconfirm --windowed --onefile --name wormenpracticum --add-data "app/window:app/window" main.py
