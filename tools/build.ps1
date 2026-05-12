48c5cc7698c11cedaf41f4c838ccee5428dfbc70
teuncm <teun.c.mathijssen@gmail.com>
2026-05-12 17:38:57 +0200

    Add metadata for packages

--- tools/build.ps1
+++ tools/build.ps1
@@ -1,2 +1,2 @@
 # Windows build script
-uv run pyinstaller --icon app/window/icon.ico --noconfirm --windowed --onefile --name wormenpracticum --add-data "app/window;app/window" main.py
+uv run pyinstaller --icon app/window/icon.ico --copy-metadata nitypes --copy-metadata nitypes --noconfirm --windowed --onefile --name wormenpracticum --add-data "app/window;app/window" main.py
